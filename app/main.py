67751542e269c31c74b5b3d6b7af89469e096a78
0xAgamy <mohamedelagamy516@gmail.com>
2026-05-03 16:59:21 +0300

    update trained model

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def diacritize_text(req: TashkeelRequest):
     Endpoint to convert undiacritized Arabic text to fully diacritized text.
     """
     result_text=predictor.diacritize(text=req.text,
-                         use_beam=True, beam_size=4)
+                        beam_size=4)
    
     return {
         "original_text": req.text,
